5a4b0c135d569541bdb75c46d77b9d5da90b3db1
salbrandi <salbrandi@gmail.com>
2017-08-07 10:54:56 -0600

    Near final commit, documentation done, README updated
    Signed-off-by: salbrandi <salbrandi@gmail.com>
    
    # Conflicts:
    #	patella/__pycache__/htmlparser.cpython-36.pyc

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='patella',
@@ -10,9 +16,25 @@ setup(
     download_url='https://github.com/salbrandi/patella/archive/0.1.tar.gz',
     py_modules=['htmlparser'],
     description='A webservice for scraping files and plotting data against presidential party variation',
+    long_description=long_description,
     entry_points='''
         [console_scripts]
         patella=click_commands:patella
         ''',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
 
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers/Data Analysts',
+        'Topic :: Data Visualisation',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6.2',
+    ],
+    keywords='plotting data party presidential office term year bokeh'
 )
